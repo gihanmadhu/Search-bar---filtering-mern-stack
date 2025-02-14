d01fa31501e06106661da69e4c2ee0a73eedf38e
Gihan <gihanmadusanka875@gmail.com>
2025-02-14 22:33:05 +0530

    Added initial input filter functionality for product search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
+import { useState } from "react";
+import Nav from "./Navigation/Nav";
+import Products from "./Products/Products";
+import Recommended from "./Recommended/Recommended";
 
+//Database
+import products from "./Data/Data"
 
 function App() {
+
+  //input filter
+  const[query,setQuery] = useState("");
+
+  const handleInputChange = (event) =>{
+    setQuery(event.target.value)
+  }
+
+  const filteredItems = products.filter(product => product.title.
+    toLowerCase().indexOf(query.toLocaleLowerCase()) !== -1);
+
+    function filteredData(products, query) {
+      let filteredProducts = products;
+  
+      // Filtering Input Items
+      if (query) {
+        filteredProducts = filteredItems;
+      }
+  
+      return filteredProducts.map(
+        ({ img, title, star, reviews, prevPrice, newPrice }) => (
+          <Card
+            key={Math.random()}
+            img={img}
+            title={title}
+            star={star}
+            reviews={reviews}
+            prevPrice={prevPrice}
+            newPrice={newPrice}
+          />
+        )
+      );
+    }
+  
+    const result = filteredData(products, selectedCategory, query);
+  
   return (
     <div className="App">
-     
+     <Nav/>
+     <Products/>
+     <Recommended/>
     </div>
   );
 }
